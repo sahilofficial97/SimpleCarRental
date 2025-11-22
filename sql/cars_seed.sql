88cd5fdcfb0f63b49a77612fee55db75b78b25f2
sahilofficial97 <91385151+sahilofficial97@users.noreply.github.com>
2025-11-22 15:18:49 +0100

    add sql file

---
+++ sql/cars_seed.sql
@@ -0,0 +1,24 @@
+-- Seed data for `car` table
+-- Ensure you are connected to the correct database before running this file.
+-- Columns: id (auto), brand_name, seat_amount, color, type
+
+INSERT INTO car (brand_name, seat_amount, color, type, price_per_day) VALUES
+  ('Toyota Camry', 5, 'silver', 'sedan', 59.00),
+  ('Honda Civic', 5, 'blue', 'sedan', 55.00),
+  ('Tesla Model 3', 5, 'white', 'sedan', 120.00),
+  ('BMW 3 Series', 5, 'black', 'sedan', 95.00),
+  ('Audi A4', 5, 'gray', 'sedan', 92.00),
+  ('Chrysler Pacifica', 7, 'white', 'minivan', 85.00),
+  ('Toyota Sienna', 8, 'red', 'minivan', 89.00),
+  ('Kia Carnival', 8, 'blue', 'minivan', 80.00),
+  ('Honda Odyssey', 8, 'silver', 'minivan', 84.00),
+  ('Mazda MX-5 Miata', 2, 'red', 'convertible', 110.00),
+  ('BMW Z4', 2, 'white', 'convertible', 135.00),
+  ('Porsche 718 Boxster', 2, 'yellow', 'convertible', 180.00),
+  ('Ford Mustang Convertible', 4, 'black', 'convertible', 125.00),
+  ('Mercedes C-Class', 5, 'white', 'sedan', 98.00),
+  ('Hyundai Sonata', 5, 'blue', 'sedan', 58.00);
+
+-- Optional sanity checks
+-- SELECT COUNT(*) AS total_cars FROM car;
+-- SELECT type, COUNT(*) AS count_by_type FROM car GROUP BY type;
